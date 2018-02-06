1c7bfbed69dc73aced99e5b3d94e8ce06aa56b3d
Denis Z(CEO) <ds.happydeveloper@gmail.com>
2018-02-07 00:08:41 +0200

    ShopScene2 Added
    The second Scene of the snake store has been added (all the UI is atteched).

---
+++ SnakeTest/Assets/imagetransperant.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class imagetransperant : MonoBehaviour {
+
+    public float alphaLevel = 1f;
+    // Use this for initialization
+    void Start () {
+        GetComponent<Image>().color = new Color(1, 1, 1, alphaLevel);
+
+    }
+
+
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
+
